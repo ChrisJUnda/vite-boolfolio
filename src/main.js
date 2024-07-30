84cad6301fe18a3a7b5ee280d498156df71580ba
ChrisJUnda <162154454+ChrisJUnda@users.noreply.github.com>
2024-07-30 18:26:41 +0200

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,28 @@
+import { createApp } from 'vue'
+import './scss/style.scss'
+
+// Import our custom CSS
+// import '../scss/styles.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+//Font Awesome
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faUserSecret);
+
+import App from "./App.vue";
+
+createApp(App)
+    .component('font-awesome-icon', FontAwesomeIcon)
+    .mount('#app')
