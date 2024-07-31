051960394058d0d5e64b20683c7089a13bf62b12
ChrisJUnda <christian.segoviaunda@gmail.com>
2024-07-31 09:44:43 +0200

    [UPD] Milestone 4

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,62 @@
-<script setup>
+<script>
+
+import axios from 'axios';
+
+export default {
+  name: 'Posts',
+  data() {
+    return {
+      title: 'Posts',
+    };
+  },
+  methods: {
+    testLifeCycle() {
+      console.log('prova ciclo di vita di vue');
+    },
+    getPosts() {
+      const result = axios.get('http://127.0.0.1:8000/api/posts').then((response) => {
+        console.log(response);
+
+        if (response.data.status && response.data.results.length) {
+          console.log('gestisco i risultati dela riposta');
+          console.log(response.data.results);
+        } else {
+          console.log('qualcosa è andato storto');
+        }
+      })
+      // console.log(result);
+      .catch((error) => console.log(error));
+  }
+},
+created() {
+  this.testLifeCycle();
+  this.getPosts();
+},
+};
 
 </script>
 
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-12">
-        <h1>
-          app
-        </h1>
-      </div>
-      <div class="col"><a href="">Colonna <font-awesome-icon :icon="['fas', 'user-secret']" /> </a></div>
-      <div class="col"><a href="">Colonna</a></div>
-      <div class="col"><a href="">Colonna</a></div>
-      <div class="col"><a href="">Colonna</a></div>
-      <div class="col"><a href="">Colonna</a></div>
-      <div class="col"><a href="">Colonna</a></div>
-      <div class="col"><a href="">Colonna</a></div>
-      <div class="col"><a href="">Colonna</a></div>
+  <header>
+    <div class="container">
+      Header
     </div>
 
-    <ul>
+  </header>
+  <main>
+    <div class="container text-center">
+      <h1>
+        {{ title }}
+      </h1>
+    </div>
+  </main>
+  <footer>
+    <div class="container">
+      Footer
 
-    </ul>
+    </div>
 
-  </div>
+  </footer>
 
 
 </template>
